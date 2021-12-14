6717af058bf63e6ae0e49851142f7e9b1178a4c3
runzac <alessandrorunza@icloud.com>
2021-12-14 21:00:22 +0100

    fix that

--- exercices-new/dowhile/14-dic/es3.cpp
+++ exercices-new/dowhile/14-dic/es3.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
-#include <cmath>
 /* Inserire due numeri interi da tastiera: a, b. 
 Il programma a questo punto deve chiedere all’utente 
 di inserire a valori interi e verificare quanti di questi sono maggiori di  b e quanti sono minori di b. */
 using namespace std;
 
 int main(){
-    int a, b, n;
-    int i = 0;
-    int massimo, minimo;
+    int a, b, max = 0, min = 0, i = 0;
 
-    cout<<"Inserisci due numeri"<<endl;
-        cin>>a>>b;
+    cout<<"inserire un valore (A)"<<endl;
+        cin>>a;
 
-    while(i<=a){
+    cout<<"inserisci un numero (B)"<<endl;
+        cin>>b;
 
-        cout<<"Inserisic quali numeri devi verificare"<<i<<endl;
-            cin>>n;
-        if(n>=b){
-            massimo = fmax(n,b);
+    while(i<a){
+        if(a>b){
+            cout<<"il numero da lei inserito e' maggiore del numero precedente"<<endl;
+            max++;
         }
         else{
-            minimo = fmin(n,b);
+            cout<<"il numero da lei inserito e' minore del numero precedente"<<endl;
+            min++;
         }
-            i++;
+        i++;
     }
 
-    cout<<"Valore massimo di b: "<<massimo<<endl;
-    cout<<"Valore minimo di b: "<<minimo<<endl;
+    cout<<"Numeri maggiori di b: "<<max<<endl;
+    cout<<"Numeri minori di b: "<<min<<endl;
+
     return 0;
 }
