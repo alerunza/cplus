9a7f0e6f862b2b12710ddeecf245b4c00fc55e6f
runzac <alessandrorunza@icloud.com>
2022-01-05 19:32:53 +0100

    tester

---
+++ exercices-new/stringhe/teststring.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string a = "Ciao0";
+    string b;
+    
+    cout<<a<<endl;
+        cin>>b;
+    
+    a = "Ciao1";
+    
+    cout<<a+b<<endl;
+}
