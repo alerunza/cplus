06912c6c51b1ed70eb3d48936753b0401455bf92
runzac <alessandrorunza@icloud.com>
2022-01-13 13:43:24 +0100

    testing

---
+++ exercices-new/for/testing/fortest.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+
+    cout<<"Inserisci i numeri: "<<endl;
+    
+    for (int i=0;i<=10;i++){
+
+    cin>>n;
+
+        if(n%2==0){
+            cout<<n<<" Pari"<<endl;
+        }
+        else{
+            cout<<n<<" Dispari"<<endl;
+        } 
+
+    }   
+}
