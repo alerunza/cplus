40a01397495d3c0040a7cb1de7132ad1a37281c9
runzac <alessandrorunza@icloud.com>
2022-01-22 00:52:47 +0100

    puntatori test

--- exercices-new/puntatori/testing/puntest.cpp
+++ exercices-new/puntatori/testing/puntest.cpp
@@ -3,5 +3,21 @@
 using namespace std;
 
 int main(){
-    
+    int n = 3;
+
+    cout<<&n<<endl; //stampa l'indirizzo della variabile "n"
+    cout<<n<<endl; // valore variabile
+
+    int *punt; // indirizzo direttamente della variabile "punt"
+
+    cout<<&punt<<endl;
+
+    punt = &n;
+    cout<<punt<<endl;
+    cout<<*punt<<endl;
+
+    *punt = 19;
+    cout<<&n<<endl;
+    cout<<n<<endl;
+
 }
