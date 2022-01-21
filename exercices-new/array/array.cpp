061831aaa410eae3eaed4900dbffba90f1223d03
runzac <alessandrorunza@icloud.com>
2022-01-21 21:42:48 +0100

    array test

--- exercices-new/array/array.cpp
+++ exercices-new/array/array.cpp
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-int main(){
+/*int main(){
      int numeri[6] = {10, 20, 30};
      string nomi[2] = {"Simone", "Stefano"}; // 0, 1 (posti nomi/ il primo è sempre 0)
 
 /*      cout<<nomi[0]<<endl;
      nomi[0] = "San Michele";
      cout<<nomi[0]<<endl;
-     nomi[0] = "Simone"; */
+     nomi[0] = "Simone"; --
 
      for(int i = 0; i<3; i++){
           cout<<numeri[i]<<endl;
@@ -26,4 +26,20 @@ int main(){
           cout<<numeri[i]<<endl;
      }
 
+}*/
+
+int main(){
+     int array[4] = {2, 3, 8, 1};
+     string nomi[4] = {"Simone", "Gero"};
+/*      cout<<array[3]<<endl;
+     array[3] = 3;
+     cout<<array[3]<<endl; */
+/* 
+     for(int i = 0; i<4; i++){
+          cout<<array[i]<<endl;
+     } */
+     nomi[3] = "Michele";
+     for(int i = 0; i<4; i++){
+          cout<<nomi[i]<<endl;
+     }
 }
