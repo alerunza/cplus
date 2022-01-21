106f971f66d2f1f6a4b6dbac9e41c65ddc338270
runzac <alessandrorunza@icloud.com>
2022-01-21 21:56:47 +0100

    Libreria chubs.h (somma)

---
+++ exercices-new/funzioni/testing/chubs.h
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int somma(int x, int y){
+
+    int somma;
+
+    somma = x + y;
+
+    cout<<somma<<endl;
+
+    return somma;
+}
--- exercices-new/funzioni/testing/funztest.cpp
+++ exercices-new/funzioni/testing/funztest.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include "chubs.h"
 
 using namespace std;
 
-int somma(int n1, int n2){
+/* int somma(int n1, int n2){
     int s;
 
     s = n1 + n2;
 
     cout<<s<<endl;
-}
+} */
 
 int main(){
     int pippo, pippi;
