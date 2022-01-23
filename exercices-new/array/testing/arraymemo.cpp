fdd26219daf9fc9c886cac9ff7bf4f927dc0a6db
runzac <alessandrorunza@icloud.com>
2022-01-23 03:02:21 +0100

    Array things

--- exercices-new/array/testing/arraymemo.cpp
+++ exercices-new/array/testing/arraymemo.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main(){
     int a[3];
 
-    for(int i = 0; i<3; i++){
+    for(int i = 0; i<3; i++){ // ciclo memorizzare
         a[i] = i;
     }
-    for(int i = 0; i<3; i++){
+    for(int i = 0; i<3; i++){ // ciclo per stampare
         cout<<a[i]<<endl;
     }
 }
