f3ec9743ad8a575a6891be1f8bee501515c83b6e
runzac <alessandrorunza@icloud.com>
2022-01-24 11:46:44 +0100

    array ordine

--- exercices-new/array/testing/arraymemo.cpp
+++ exercices-new/array/testing/arraymemo.cpp
@@ -5,10 +5,18 @@ using namespace std;
 int main(){
     int array[3];
 
-    for(int i = 0; i<3; i++){ // ciclo memorizzare
+/*     for(int i = 0; i<3; i++){ // ciclo memorizzare
         array[i] = i;
     }
     for(int i = 0; i<3; i++){ // ciclo per stampare
+        cout<<array[i]<<endl;
+    } */
+
+    for(int i = 0; i<=3; i++){
+        cin>>array[i];
+    }
+
+    for(int i = 3; i>=0; i--){ // stampa al contrario
         cout<<array[i]<<endl;
     }
 }
