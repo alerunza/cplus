ed2562560a90e77de462b1ca0e9ee5f2f63d400e
runzac <alessandrorunza@icloud.com>
2022-01-26 11:09:22 +0100

    3° array es [init] (macOS Monterey)

---
+++ exercices-new/array/gennaio/25/array3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+/* Scrivi un programma che richiede
+all'utente un vettore costituito da un numero pari di elementi (massimo 100
+elementi). Il programma deve scambiare i valori in posizione pari con quelli in
+posizione dispari, e infine stampare il contenuto del vettore dopo tale
+operazione.  */
+using namespace std;
+
+int main(){
+
+}
