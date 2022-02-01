41de17afa3db54ed80c7e91c67535b79a21c34df
runzac <alessandrorunza@icloud.com>
2022-02-01 14:43:13 +0100

    Es 1 [String] (macOS Monterey)

---
+++ exercices-new/stringhe/febbraio/01/es1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+/* 1 - Leggi una stringa e determina quanto è lunga. */
+using namespace std;
+
+int main(){
+
+     string n = "ciao";
+
+     cout<<n.length()<<endl;
+
+}
