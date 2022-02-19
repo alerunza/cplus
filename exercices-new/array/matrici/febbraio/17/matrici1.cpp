d526373855283eff65eb0747d201aeb0f920d806
runzac <alessandrorunza@icloud.com>
2022-02-19 11:57:43 +0100

    Matrici fix [totale di ogni riga & ogni colonna]

--- exercices-new/array/matrici/febbraio/17/matrici1.cpp
+++ exercices-new/array/matrici/febbraio/17/matrici1.cpp
@@ -15,25 +15,35 @@ int main(){
 
      int matrice[r][c];
      for(i = 0 ; i < r ; i++ ){
-          sommarighe = 0;
-          sommacolonne = 0;
           for(j = 0 ; j < c ; j++){
                cout<<"Inserisci i numeri all'interno della matrice"<<endl;
                     cin>>matrice[i][j];
-               sommarighe += matrice[i][j]; //1- il totale per ogni riga della matrice
-               sommacolonne += matrice[j][i]; //2- il totale per ogni colonna della matrice
           }
      }
-
+     
      for(i = 0 ; i < r ; i++ ){
           for(j = 0 ; j < c ; j++){
                cout<<matrice[i][j]<<"\t";   //stampa l'intera matrice
           }
           cout<<endl;
      }
-     
-     cout<<"la SOMMA delle righe della MATRICE: "<<sommarighe<<endl;
-     cout<<"la SOMMA delle colonne della MATRICE: "<<sommacolonne<<endl;
-     
+
+     for(i = 0 ; i < r ; i++ ){ // somma righe
+          sommarighe = 0;
+          for(j = 0 ; j < c ; j++){
+               sommarighe += matrice[i][j]; //1- il totale per ogni riga della matrice
+          }
+          //cout<<endl;
+          cout<<"la SOMMA delle righe della MATRICE: "<<sommarighe<<endl;
+     }
+
+     for(i = 0 ; i < r ; i++ ){ // somma colonne
+          sommacolonne = 0;
+          for(j = 0 ; j < c ; j++){
+               sommacolonne += matrice[j][i]; //2- il totale per ogni colonna della matrice
+          }
+          cout<<"la SOMMA delle colonne della MATRICE: "<<sommacolonne<<endl;
+     }
+
      return 0;
 }
