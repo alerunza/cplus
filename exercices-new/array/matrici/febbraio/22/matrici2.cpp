ab7f27807f937bbe3e66d9a7a4a775d2498a7814
runzac <alessandrorunza@icloud.com>
2022-02-22 12:48:19 +0100

    ES 2 [matrici] (init)

---
+++ exercices-new/array/matrici/febbraio/22/matrici2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+/*Si devono organizzare gli incassi di 20 reparti di un grande magazzino nei 6
+giorni della settimana. Si vuole calcolare l'incasso
+totale di tutti i reparti nel quarto giorno della settimana */
+using namespace std;
+
+int main(){
+    int i, j; // i = n.righe; j = n.colonne
+    int somma;
+    int matrice[20][6];
+    
+    for(i = 0 ; i < 20 ; i++ ){
+        for(j = 0 ; j < 6 ; j++){
+            cout<<"Inserisci gli incassi della settimana seguente: "<<i<<" "<<j<<endl;
+                cin>>matrice[i][j];
+        }
+    }
+    
+    for(i = 0 ; i < 20 ; i++ ){
+        for(j = 0 ; j < 6 ; j++){
+            cout<<"Incassi registrati: "<<endl;
+            cout<<matrice[i][j]<<"\t";   //stampa l'intera matrice
+        }
+        cout<<endl;
+    }
+    for(i = 0 ; i < 20 ; i++ ){
+        somma = 0;
+        for(j = 0 ; j < 4 ; j++){    //calcolo totale degli incassi del 4 giorno
+            somma += matrice[i][j];
+        }
+        cout<<endl;
+    }
+    cout<<somma<<endl;
+}
