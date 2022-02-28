f9bb1d6fab59da98606c0e56214350fba2b398a0
runzac <alessandrorunza@icloud.com>
2022-02-28 19:17:56 +0100

    init ES 5 (studente assenze)

---
+++ exercices-new/array/matrici/febbraio/28/matrici5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+/* 5 - Programma che, date le assenze di uno studente ciascuno
+dei primi 6 mesi dell'anno scolastico, determina il mese in cui se ne sono
+verificate di più. */
+using namespace std;
+
+int main(){
+    int studente[6]; // 6 = 6 mesi
+
+    cout<<"Inserisci per ogni mese (6), il numero di assenze che ha fatto in quel mese"<<endl;
+    for(int i = 0; i < 6; i++){
+        cin>>studente[i];
+    }
+    for(int i = 0; i < 6; i++){
+        if(studente[i]>)
+    }
+}
