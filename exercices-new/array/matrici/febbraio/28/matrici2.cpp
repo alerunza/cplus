85fc9290319154afae89da2e5c9006cae020789a
runzac <alessandrorunza@icloud.com>
2022-02-28 19:40:03 +0100

    init ES 2 (pre compito)

---
+++ exercices-new/array/matrici/febbraio/28/matrici2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+/*2 - Dato un array A[ N ][ M] di elementi reali, produrre:
+Le somme di ogni riga
+Le somme di ogni colonna
+La riga di somma massima
+La colonna di somma massima
+
+N=4
+M=4 
+1 2 3 4
+5 6 7 8 
+9 1 2 3 
+4 5 6 7 
+Somme righe 10 26 15 22 
+Somme colonne 19 14 18 22 
+Riga max 1 
+Colonna min 3*/
+using namespace std;
+
+int main(){
+    int N = 4;
+    int M = 4;
+    int A[N][M];
+    
+}
