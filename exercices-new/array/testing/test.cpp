9379c77b0630fcf61c61a0cfff2e2f1091c5ffbb
runzac <alessandrorunza@icloud.com>
2022-03-04 16:36:23 +0100

    michele non sa programmare

---
+++ exercices-new/array/testing/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+      int i = 0 ;
+
+      while ( i == 0 ){
+           cout << " Sono arrogante " << endl;
+           cout << endl;
+           cout << " Turco Michele sa programmare " << endl;
+      }
+}
+/* Funziona come java  */
