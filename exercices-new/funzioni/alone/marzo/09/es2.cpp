e4e2680829a087fac95574453e305a04a2cb5413
runzac <alessandrorunza@icloud.com>
2022-03-09 19:45:30 +0100

    init es 2

---
+++ exercices-new/funzioni/alone/marzo/09/es2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+/* Creare una funzione che riceve una quantità di tempo in formato ore, 
+minuti e secondi e la restituisce espressa solamente in secondi.
+Creare poi un programma che si faccia passare due quantità di tempo e 
+verifichi quale è maggiore utilizzando la funzione precedente e solamente due if nel programmaa. */
+using namespace std;
+
+int main(){
+
+}
