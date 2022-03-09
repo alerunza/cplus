5e7047069e2a41ff5970c52d8e506d49051cc55f
runzac <alessandrorunza@icloud.com>
2022-03-09 19:44:36 +0100

    Ending es 1

--- exercices-new/funzioni/alone/marzo/09/es1.cpp
+++ exercices-new/funzioni/alone/marzo/09/es1.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
-int main(){
+int valass(int x){
+    if(x<0){
+        x=-x;
+    }
     
+    return x;
+}
+
+int main(){
+
+    int num;
+
+    cout<<"Inserisci un numero"<<endl;
+        cin>>num;
+    cout<<"VALORE Assoluto: "<<valass(num)<<endl;
 }
