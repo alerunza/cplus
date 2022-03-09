eb53b43f6be2683ba9b34297011aa9a88de182a2
runzac <alessandrorunza@icloud.com>
2022-03-09 20:05:39 +0100

    es 2 done

--- exercices-new/funzioni/alone/marzo/09/es2.cpp
+++ exercices-new/funzioni/alone/marzo/09/es2.cpp
@@ -5,14 +5,23 @@ Creare poi un programma che si faccia passare due quantità di tempo e
 verifichi quale è maggiore utilizzando la funzione precedente e solamente due if nel programmaa. */
 using namespace std;
 
-void tempoinsec(){
-    int ore, minuti, secondi;
+int tempoinsec(int ore, int minuti, int secondi){
     cout<<"Inserisci ore, minuti e secondi"<<endl;
         cin>>ore>>minuti>>secondi;
-    int totale = ore*3600 + minuti*60 + secondi;
-    cout<<"Totale in secondi: "<<totale<<endl;
+    cout<<secondi+minuti*60+ore*3600<<endl;
 }
 
 int main(){
-    tempoinsec();
+    
+    int ore, minuti, secondi;
+    int ore2, minuti2, secondi2;
+    
+    int tempo1 = tempoinsec(ore, minuti, secondi);
+    int tempo2 = tempoinsec(ore2, minuti2, secondi2);
+    if(tempo1>tempo2){
+        cout<<"Il PRIMO quantitativo di tempo e' MAGGIORE del SECONDO"<<endl;
+    }
+    if(tempo1<tempo2){
+        cout<<"Il SECONDO quantitativo di tempo e' MAGGIORE del PRIMO"<<endl;
+    }
 }
