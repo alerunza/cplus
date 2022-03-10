4174982da120c83e8e742c5ead58003487d7fb2e
runzac <alessandrorunza@icloud.com>
2022-03-10 07:08:07 +0100

    stoi function work (media/somma italiano)

--- exercices-new/array/matrici/marzo/09-classe/tabella1.cpp
+++ exercices-new/array/matrici/marzo/09-classe/tabella1.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -24,4 +27,23 @@ int main(){
           cout<<endl;
      }
 
+/*      int somma[3];
+     int ciao[3];
+     for(int i = 0; i < 4; i++){
+          somma[0] += stoi(tabella[i][2]);
+          somma[1] += stoi(tabella[i][3]);
+          somma[2] += stoi(tabella[i][4]);
+          
+     }
+     cout<<somma[0]<<endl;
+     cout<<somma[1]<<endl;
+     cout<<somma[2]<<endl; */
+     
+     float voti = 0;
+     float media = 0;
+     for(int i = 1; i < 5; i++){
+          voti += stoi(tabella[i][2]);
+     }
+     media = voti / 4;
+     cout<<"la MEDIA della classe di ITALIANO e' "<<media<<endl;
 }
