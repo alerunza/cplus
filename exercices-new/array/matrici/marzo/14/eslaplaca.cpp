652333b0d26529753d5d09aadb613bd9112361d2
runzac <alessandrorunza@icloud.com>
2022-03-14 11:51:09 +0100

    init programma di la placa, svolto in classe

---
+++ exercices-new/array/matrici/marzo/14/eslaplaca.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+     int righe, colonne;
+
+     cout<<"Inserisci le righe e le colonne"<<endl;
+          cin>>righe>>colonne;
+          
+     int matrice[righe][colonne];
+     int matrice2[righe][colonne];
+
+
+}
