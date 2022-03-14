cde1565b3130aa8a69d3136ddd91f81a012dbb17
runzac <alessandrorunza@icloud.com>
2022-03-14 11:57:46 +0100

    init es laplaca & frisina

--- exercices-new/array/matrici/marzo/14/eslaplaca.cpp
+++ exercices-new/array/matrici/marzo/14/eslaplaca.cpp
@@ -4,12 +4,36 @@ using namespace std;
 
 int main(){
      int righe, colonne;
+     int r, c;
+     int r2, c2;
 
      cout<<"Inserisci le righe e le colonne"<<endl;
           cin>>righe>>colonne;
-          
+
      int matrice[righe][colonne];
      int matrice2[righe][colonne];
 
-
+     for(r = 0; r<righe; r++){
+          for(c = 0; c<colonne; c++){
+               cin>>matrice[r][c];
+          }
+     }
+     for(r = 0; r<righe; r++){
+          for(c = 0; c<colonne; c++){
+               cout<<matrice[r][c]<<"\t";
+          }
+          cout<<endl;
+     }
+     cout<<endl;
+     for(r = 0, c2 = 0; r<righe; c2++, r++){
+          for(c = 0, r2 = 0; c<colonne; r2++, c++){
+               matrice2[r2][c2] = matrice[r][c];
+          }
+     }
+     for(r = 0; r<righe; r++){
+          for(c = 0; c<colonne; c++){
+               cout<<matrice2[r][c]<<"\t";
+          }
+          cout<<endl;
+     }
 }
