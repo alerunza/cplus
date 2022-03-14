bedf6ef65c93880659a609a525f15e671f769d4a
runzac <alessandrorunza@icloud.com>
2022-03-14 12:07:13 +0100

    fix cout

--- exercices-new/array/matrici/marzo/14/esrunza.cpp
+++ exercices-new/array/matrici/marzo/14/esrunza.cpp
@@ -24,12 +24,12 @@ int main(){
           cout<<endl;
      }
      cout<<endl;
-
      for(i = 0; i < righe; i++){
           for(j = 0; j < colonne; j++){
                matrice2[j][i]=matrice[i][j];
           }
      }
+     cout<<"Matrice trasportata"<<endl;
      for (i = 0; i < colonne; i++) {
           for(j = 0; j < righe; j++){
                cout<<matrice2[i][j]<<"\t";
