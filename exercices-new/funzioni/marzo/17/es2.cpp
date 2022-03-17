a9d39681cd6230123f325bbf1446263db597a5b1
runzac <alessandrorunza@icloud.com>
2022-03-17 15:18:09 +0100

    Es 2 (Dati tre numeri in input ordinali in modo crescente utilizzando le funzioni)

---
+++ exercices-new/funzioni/marzo/17/es2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+/* Dati tre numeri in input ordinali in modo crescente utilizzando le funzioni */
+using namespace std;
+
+void ordine(int x, int y, int z){
+    
+    if(x<y){
+        if(x<z){
+            if(y<z){
+                cout<<x<<" "<<y<<" "<<z<<endl;
+            }
+            else
+                cout<<x<<" "<<z<<" "<<y<<endl;
+        }
+    }else
+        if(x<z){
+            cout<<y<<" "<<x<<" "<<z<<endl;
+        }else
+            if(y<z){
+                cout<<y<<" "<<z<<" "<<x<<endl;
+            }
+            else
+                cout<<y<<" "<<z<<" "<<x<<endl;
+}
+
+int main(){
+    int a, b, c;
+
+    cout<<"Inserisci 3 numeri"<<endl;
+        cin>>a>>b>>c;
+
+    ordine(a, b, c);
+}
