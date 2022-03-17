f913942e181da9fe80d178a1b0945c322ce42b4c
runzac <alessandrorunza@icloud.com>
2022-03-17 15:39:22 +0100

    Es 3 prototipo con return

--- exercices-new/funzioni/marzo/17/es3prototipo.cpp
+++ exercices-new/funzioni/marzo/17/es3prototipo.cpp
@@ -6,22 +6,34 @@ using namespace std;
 
 float a, b, c; // dichiarazione di variabili globali
 
-void eqsecgrado(float a, float b, float c);
+float eqsecgrado(float a, float b, float c);
+float eqsecgrado2(float a, float b, float c);
 
 int main(){
+    float x1, x2;
     cout<<"Inserisci un Equazione di SECONDO Grado"<<endl;
         cin>>a>>b>>c;
 
-    eqsecgrado(a, b, c);
+    x1 = eqsecgrado(a, b, c);
+    x2 = eqsecgrado2(a, b, c);
     
+    cout<<x1<<" "<<x2<<endl;
+
     return 0;
 }
 
-void eqsecgrado(float a, float b, float c){
-    float x1, x2; // dichiarazione di variabili locali
+float eqsecgrado(float a, float b, float c){
+    float x1; // dichiarazione di variabili locali
 
     x1 = (-b - sqrt ((b*b) - 4*a*c))/(2*a); // procedimento per la risoluzione dell'equazione di sec grado.
-    x2 = (-b + sqrt ((b*b) - 4*a*c))/(2*a);
 
-    cout<<x1<<" "<<x2<<endl; // stampa
+    return x1;
+}
+
+float eqsecgrado2(float a, float b, float c){
+    float x2; // dichiarazione di variabili locali
+
+    x2 = (-b + sqrt ((b*b) - 4*a*c))/(2*a);// procedimento per la risoluzione dell'equazione di sec grado.
+
+    return x2;
 }
