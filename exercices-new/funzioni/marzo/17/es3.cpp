7e4b2a0509680b744d14342457e18ae5201e0cd5
runzac <alessandrorunza@icloud.com>
2022-03-17 15:25:09 +0100

    Es 3 (eq sec grado)

---
+++ exercices-new/funzioni/marzo/17/es3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+/* Implementare le soluzioni
+di un'equazione di secondo grado utilizzando variabili locali e globali */
+using namespace std;
+
+float a, b, c; // dichiarazione di variabili globali
+
+void eqsecgrado(float a, float b, float c){
+    float x1, x2; // dichiarazione di variabili locali
+
+    x1 = (-b - sqrt ((b*b) - 4*a*c))/(2*a); // procedimento per la risoluzione dell'equazione di sec grado.
+    x2 = (-b + sqrt ((b*b) - 4*a*c))/(2*a);
+
+    cout<<x1<<" "<<x2<<endl; // stampa
+}
+
+int main(){
+    cout<<"Inserisci un Equazione di SECONDO Grado"<<endl;
+        cin>>a>>b>>c;
+
+    eqsecgrado(a, b, c);
+    
+    return 0;
+}
