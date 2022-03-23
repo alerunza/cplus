be7b230b81d0dfa468185a7187767540b63e3ac2
runzac <alessandrorunza@icloud.com>
2022-03-23 11:58:27 +0100

    matrice failed to complete

--- exercices-new/funzioni/marzo/18/esmichrunzac.cpp
+++ exercices-new/funzioni/marzo/18/esmichrunzac.cpp
@@ -4,21 +4,19 @@ using namespace std;
 
 int i, j;
 
-void carica(int r, int c);
-void stampa(int r, int c);
+void carica(int matrice, int &r, int &c);
+void stampa(int matrice, int &r, int &c);
 
 int main(){
-    int righe, colonne;
-    
-    cout<<"Inserisci un NUMERO per DETERMINARE una MATRICE"<<endl;
-        cin>>righe>>colonne;
-
-    carica(righe, colonne);
-    stampa(righe, colonne);
+    int *puntatore;
+    int dimensione;
+    carica(puntatore, dimensione, dimensione);
+    stampa(puntatore, dimensione, dimensione);
 }
 
-void carica(int r, int c){
-    int matrice[r][c];
+void carica(int matrice, int &r, int &c){
+    cout<<"Inserisci un NUMERO per DETERMINARE una MATRICE"<<endl;
+        cin>>r>>c;
     for(i = 0 ; i < r ; i++ ){
         for(j = 0 ; j < c ; j++){
             cout<<"Inserisci i numeri all'interno della matrice"<<endl;
@@ -26,7 +24,7 @@ void carica(int r, int c){
         }
     }   
 }
-void stampa(int r, int c){
+void stampa(int matrice, int &r, int &c){
     int matrice[r][c];
     for(i = 0 ; i < r ; i++ ){
         for(j = 0 ; j < c ; j++){
