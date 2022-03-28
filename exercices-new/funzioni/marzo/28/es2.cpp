8851a0243672b633b2cef12203ed6a887479091f
runzac <alessandrorunza@icloud.com>
2022-03-28 13:43:05 +0200

    added text + definition of val assoluto

---
+++ exercices-new/funzioni/marzo/28/es2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+/* Definisci una funzione che restituisce il valore assoluto di un numero reale passato come argomento. */
+using namespace std;
+
+/* Il valore assoluto di un numero è 
+uguale al numero stesso se il numero è positivo o nullo, // DEFINIZIONE DI  Valore Assoluto
+è l'opposto del numero se il numero è negativo. */
+
+
+int main(){
+
+}
