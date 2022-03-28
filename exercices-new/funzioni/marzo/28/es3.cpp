f9d91eba9870c56b5e1a1778b1f4a18c4c712fbe
runzac <alessandrorunza@icloud.com>
2022-03-28 13:58:57 +0200

    ES 3 (28/03/2022) [Massimo] DONE

--- exercices-new/funzioni/marzo/28/es3.cpp
+++ exercices-new/funzioni/marzo/28/es3.cpp
@@ -2,6 +2,24 @@
 /* Definisci una funzione che calcola il massimo fra due numeri interi. */
 using namespace std;
 
+int massimo(int x, int y){
+     int max;
+     if(x>y){
+          max = x;
+     }
+     if(x<y){
+          max = y;
+     }
+
+     return max;
+}
+
 int main(){
+     int n1, n2;
+
+     cout<<"Inserisci due NUMERI"<<endl;
+          cin>>n1>>n2;
+     
+     cout<<"Il MASSIMO tra i due NUMERI e' "<<massimo(n1, n2)<<endl;
 
 }
