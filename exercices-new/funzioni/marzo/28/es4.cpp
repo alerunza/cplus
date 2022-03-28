3599f3638407a2e74af6273e144f2984d2da1eac
runzac <alessandrorunza@icloud.com>
2022-03-28 15:51:07 +0200

    ES 4 (28/03/2022) Check NUM Primo (false/true)

---
+++ exercices-new/funzioni/marzo/28/es4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+/* Definisci una funzione che riceve in ingresso un numero naturale 
+e restituisce il valore true se il numero e' primo, e false se non lo e'. */
+using namespace std;
+
+//un numero primo deve essere divisibile per se stesso e per 1
+
+bool checkprimo(int n){
+     int divisore = 2;
+     while(divisore < n){
+          if(n % divisore == 0){
+               return false;
+          }
+          divisore++;
+     }
+     return true;
+}
+
+int main(){
+     int numero;
+     
+     cout<<"Inserisci un NUMERO per verificare se e' un numero PRIMO."<<endl;
+          cin>>numero;
+
+     if(checkprimo(numero)){ 
+          cout<<"Il NUMERO: "<<numero<<" e' primo"<<endl;
+     }else{
+          cout<<"Il NUMERO: "<<numero<<" non e' primo"<<endl;
+     }
+
+}
