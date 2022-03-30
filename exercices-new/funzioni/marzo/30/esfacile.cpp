4870a3238545af34dc8d4cc34b4bc210b6e6c3ea
runzac <alessandrorunza@icloud.com>
2022-03-30 11:52:27 +0200

    init es 1 facile

---
+++ exercices-new/funzioni/marzo/30/esfacile.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+/* Versione uno facilitata:
+Creare un programma che preveda 5 estrazioni di due dadi a 6 facce 
+che vengano memorizzate in un vettore. */
+using namespace std;
+
+void estrazione(int array[]){
+     for(int i = 0; i < 5; i++){
+          array[i] = rand()%6+1;
+     }
+     for(int i = 0; i < 5; i++){
+          cout<<"Punti ottenuti: "<<array[i]<<endl;
+     }
+}
+
+int main(){
+     srand(time(NULL)); // seed per inizializzare il generatore di numeri.
+
+     int array[5];
+
+     estrazione(array);
+
+     for(int i = 0; i < 5; i++){
+          cout<<"Punti ottenuti dopo la memorizzazione: "<<array[i]<<endl;
+     }
+}
