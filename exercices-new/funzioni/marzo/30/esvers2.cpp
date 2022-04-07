f637c703546cfeb75d05955c45a1511b209e081e
runzac <alessandrorunza@icloud.com>
2022-04-07 12:56:26 +0200

    reworked es 2

--- exercices-new/funzioni/marzo/30/esvers2.cpp
+++ exercices-new/funzioni/marzo/30/esvers2.cpp
@@ -9,24 +9,26 @@ using namespace std;
 // tot dadi andranno ad effettuare la somma tra le estrazione per il numero di dadi
 void estrazione(int array[], int lanci, int dadi){
      srand(time(NULL)); // seed per inizializzare il generatore di numeri.
-     int somma = 0, k = 0;
+     int somma = 0, k = 0, b = 0, l = 0;
+     int sommadadi = 0;
      int array2[lanci*dadi];
      for(int x = 1; x <= dadi; x++){
           for(int i = 0; i < lanci; i++){
                array[i] = rand()%6+1; // 6 facce
                array2[k] = array[i];
+               sommadadi += array2[k];
                k++;
           }
           for(int j = 0; j < lanci; j++){
                cout<<"Punti ottenuti: "<<array[j]<<endl;
           }
+          /* cout<<"SOMMA del "<<x<<" dado: "<<sommadadi; */
+          cout<<"sommadadi: "<<sommadadi<<endl;
+          sommadadi = 0;
           cout<<endl;
      }
      for(int z = 0; z < lanci*dadi; z++){
-               /* cout<<array2[z]<<endl; */
                somma += array2[z];
-               /* cout<<somma<<endl;
-               cout<<endl; */
      }
      cout<<"Punti TOTALI: "<<somma<<endl;
 }
