4619335e174c7d1eebf79f73d3565ee30b85ef63
runzac <alessandrorunza@icloud.com>
2022-05-05 13:04:34 +0200

    update

--- exercices-new/funzioni/intplaca.cpp
+++ exercices-new/funzioni/intplaca.cpp
@@ -3,17 +3,37 @@
 Creare una programma che applica la funzione a tre valori inseriti dall’utente. */
 using namespace std;
 
-void funzione(float n1, float n2, float n3);
+float valore(float a, float b, float c);
 
 int main(){
-     int a, b, c;
+     int a,b,c;
 
-     cout<<"Inserisci 3 NUMERI"<<endl;
+     cout<<"inserisci tre  valori"<<endl;
           cin>>a>>b>>c;
 
-     funzione(a, b, c);
+     valore(a,b,c);
+    
+     return 0;
 }
-void funzione(float n1, float n2, float n3){
+float valore(float a, float b, float c){
+     float massimo=0,min=0,finale=0; // 7 2 9
      
-     cout<<n1<<" "<<n2<<" "<<n3<<endl;
+     if(a>b)
+     massimo=a;
+     else
+     massimo=b;
+     if(massimo<c)
+     massimo=c;
+
+     if(a<b)
+     min=a;
+     else
+     min=b;
+     if(min>c)
+     min=c;
+     min=min/2;
+     finale=massimo-min;
+     
+     cout<<"valore finale : "<<finale<<endl;
+
 }
