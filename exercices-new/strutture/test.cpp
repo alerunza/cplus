aaf45a984a1f8fef40ee75d65030edc0360214c5
chubs <alessandrorunza@icloud.com>
2022-05-17 16:18:47 +0200

    ES Struttura (fixed)

--- exercices-new/strutture/test.cpp
+++ exercices-new/strutture/test.cpp
@@ -1,30 +1,36 @@
 #include <iostream>
-/* Calcolare la differenza in minuti fra due orari utilizzando le strutture */
+/* Calcolare la differenza in tempo fra due orari utilizzando le strutture */
 using namespace std;
 
 struct orario{
      int ora;
      int ora2;
+     int minuti;
+     int minuti2;
 };
 
 int main(){
-     orario minuti;
+     orario tempo;
      
-     cout<<"Inserisci due orari ES: (15 & 19)"<<endl;
-          cin>>minuti.ora>>minuti.ora2;
+     cout<<"Inserisci due orari ES: (15:40 & 19:30)"<<endl;
+          cin>>tempo.ora>>tempo.minuti;
+     cout<<"Primo ORARIO: "<<tempo.ora<<":"<<tempo.minuti<<"\nInserisci il secondo orario"<<endl;
+          cin>>tempo.ora2>>tempo.minuti2;
+     cout<<"Secondo ORARIO: "<<tempo.ora2<<":"<<tempo.minuti2<<endl;
 
-     cout<<minuti.ora<<":00 & "<<minuti.ora2<<":00"<<endl;
+     int ora, ora2;
+     
+     ora = tempo.ora * 60 + tempo.minuti;
+     ora2 = tempo.ora2 * 60 + tempo.minuti2;
 
-     int differenza;
+     int totaleora;
 
-     if(minuti.ora>minuti.ora2){
-          differenza = minuti.ora-minuti.ora2;
+     if(ora>ora2){
+          totaleora = ora - ora2;
      }
-     if(minuti.ora<minuti.ora2){
-          differenza = minuti.ora2-minuti.ora;
+     if(ora<ora2){
+          totaleora = ora2 - ora;
      }
      
-     differenza = differenza * 60;
-
-     cout<<differenza<<" minuti"<<endl;
+     cout<<totaleora<<" minuti"<<endl;
 }
