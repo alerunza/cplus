f1b117d1ed5eec62afbe9aa249255962ecf60c4a
runzac <alessandrorunza@icloud.com>
2022-05-18 11:33:32 +0200

    funzioni con strutture

--- exercices-new/strutture/test.cpp
+++ exercices-new/strutture/test.cpp
@@ -3,23 +3,52 @@
 /* Calcolare la differenza in minuti fra due orari utilizzando le strutture */
 using namespace std;
 
+void carica_array(int dimensione);
+void stampa_array(int dimensione);
+int min_array(int dimensione);
+
 struct orario{
      int ora;
      int minuti;
 };
 
+orario tempo[5];
+
 int main(){
-     orario tempo, tempo2;
      
-     cout<<"Inserisci due orari ES: (15:40 & 19:30)"<<endl;
-          cin>>tempo.ora>>tempo.minuti;
-     cout<<"Primo ORARIO: "<<tempo.ora<<":"<<tempo.minuti<<"\nInserisci il secondo orario"<<endl;
-          cin>>tempo2.ora>>tempo2.minuti;
-     cout<<"Secondo ORARIO: "<<tempo2.ora<<":"<<tempo2.minuti<<endl;
-
-     int oratot;
+     int dim = 5;
      
-     oratot = (tempo.ora * 60 + tempo.minuti) - (tempo2.ora * 60 + tempo2.minuti);
+     cout<<"Inserisci "<< dim <<" orari ES: (15:40 & 19:30)"<<endl;
 
-     cout<<abs(oratot)<<" minuti"<<endl;
+     carica_array(dim);
+     stampa_array(dim);
+     
+     int indice = min_array(dim);
+
+     cout<<"L'orario minore e' - "<<tempo[indice].ora<<" : "<<tempo[indice].minuti<<endl;
+}
+
+void carica_array(int dimensione){
+     for(int i=0; i<dimensione; i++){
+          cin>>tempo[i].ora>>tempo[i].minuti;
+     }
+}
+
+void stampa_array(int dimensione){
+     for(int i=0; i<dimensione; i++){
+          cout<<"Orario n."<<i+1<<" - "<<tempo[i].ora<<" : "<<tempo[i].minuti<<endl;
+     }
+}
+
+int min_array(int dimensione){
+     int min = 1000000;
+     int v;
+     for(int i=0; i<dimensione; i++){
+          if(tempo[i].ora<min){
+               v = i;
+               min = tempo[i].ora;
+          }
+     }
+     
+     return v;
 }
