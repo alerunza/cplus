3a5ec667d0c941659a2f5beb6c1e2ea4ce1047fe
runzac <alessandrorunza@icloud.com>
2022-08-21 01:52:43 +0200

    Implementazione di system("CLS"); + ottimizzazione + fix cout

--- progetti/roulette/main.cpp
+++ progetti/roulette/main.cpp
@@ -18,14 +18,12 @@ int main()
 
      int saldo = 100, saldoin = 100;
      int vincita = 0;
-     char risposta;
-
-     cout << "Benvenuto nella European Roulette by chvbs & Mich24" << endl;
-     cout << "[0][3][6][9][12][15][18][21][24][27][30][33][36]\n[2][5][8][11][14][17][20][23][26][29][32][35]\n[1][4][7][10][13][16][19][22][25][28][31][34]" << endl;
-     cout << "Hai un saldo iniziale di: " << saldo << endl;
 
      do
      {
+          cout << "Benvenuto nella European Roulette by chvbs & Mich24" << endl;
+          cout << "[0][3][6][9][12][15][18][21][24][27][30][33][36]\n[2][5][8][11][14][17][20][23][26][29][32][35]\n[1][4][7][10][13][16][19][22][25][28][31][34]" << endl;
+          cout << "Hai un SALDO iniziale di: " << saldo << "$" << endl;
           int random = rand() % 2;
           cout << "Vuoi puntare su un numero specifico? (y/n)" << endl;
           cin >> roulette[0].risp;
@@ -43,7 +41,7 @@ int main()
                     cin >> roulette[1].puntate;
                     if (roulette[1].puntate > saldo)
                     {
-                         cout << "Hai puntato piu' del tuo saldo DISPONIBILE - SALDO: " << saldo << endl;
+                         cout << "Hai puntato piu' del tuo saldo DISPONIBILE - SALDO: " << saldo << "$" << endl;
                     }
                     if (roulette[1].puntate <= 0)
                     {
@@ -51,7 +49,7 @@ int main()
                     }
                } while (roulette[1].puntate > saldo or roulette[1].puntate <= 0);
                saldo = saldo - roulette[1].puntate;
-               cout << "Hai puntato: " << roulette[1].puntate << ", il tuo SALDO adesso e' " << saldo << endl;
+               cout << "Hai puntato: " << roulette[1].puntate << "$, il tuo SALDO adesso e' " << saldo << "$" << endl;
           }
           cout << "Vuoi puntare su un numero pari o dispari? (y/n)" << endl;
           cin >> roulette[1].risp;
@@ -68,7 +66,7 @@ int main()
                          cin >> roulette[2].puntate;
                          if (roulette[2].puntate > saldo)
                          {
-                              cout << "Hai puntato piu' del tuo saldo DISPONIBILE - SALDO: " << saldo << endl;
+                              cout << "Hai puntato piu' del tuo saldo DISPONIBILE - SALDO: " << saldo << "$" << endl;
                          }
                          if (roulette[2].puntate < 0)
                          {
@@ -77,7 +75,7 @@ int main()
                     } while (roulette[2].puntate > saldo or roulette[2].puntate < 0);
 
                     saldo = saldo - roulette[2].puntate;
-                    cout << "Hai puntato: " << roulette[2].puntate << ", il tuo SALDO adesso e' " << saldo << endl;
+                    cout << "Hai puntato: " << roulette[2].puntate << "$, il tuo SALDO adesso e' " << saldo << "$" << endl;
                }
                if (roulette[1].risp == 'p' or roulette[1].risp == 'P')
                {
@@ -88,7 +86,7 @@ int main()
                          cin >> roulette[2].puntate;
                          if (roulette[2].puntate > saldo)
                          {
-                              cout << "Hai puntato piu' del tuo saldo DISPONIBILE - SALDO: " << saldo << endl;
+                              cout << "Hai puntato piu' del tuo saldo DISPONIBILE - SALDO: " << saldo << "$" << endl;
                          }
                          if (roulette[2].puntate < 0)
                          {
@@ -96,16 +94,16 @@ int main()
                          }
                     } while (roulette[2].puntate > saldo or roulette[2].puntate < 0);
                     saldo = saldo - roulette[2].puntate;
-                    cout << "Hai puntato: " << roulette[2].puntate << ", il tuo SALDO adesso e' " << saldo << endl;
+                    cout << "Hai puntato: " << roulette[2].puntate << "$, il tuo SALDO adesso e' " << saldo << "$" << endl;
                }
           }
           cout << "Vuoi puntare sui numeri maggiori/minori di 18? (y/n)" << endl;
-          cin >> roulette[3].risp;
-          if (roulette[3].risp == 'y' or roulette[3].risp == 'Y')
+          cin >> roulette[2].risp;
+          if (roulette[2].risp == 'y' or roulette[2].risp == 'Y')
           {
                cout << "Cosa scegli: MAGGIORE di 18 o MINORE di 18 (e/g)" << endl;
-               cin >> roulette[3].risp;
-               if (roulette[3].risp == 'g' or roulette[3].risp == 'G')
+               cin >> roulette[2].risp;
+               if (roulette[2].risp == 'g' or roulette[2].risp == 'G')
                {
                     cout << "Hai scelto la puntata per i numeri MINORI di 18" << endl;
                     do
@@ -114,7 +112,7 @@ int main()
                          cin >> roulette[3].puntate;
                          if (roulette[3].puntate > saldo)
                          {
-                              cout << "Hai puntato piu' del tuo saldo DISPONIBILE - SALDO: " << saldo << endl;
+                              cout << "Hai puntato piu' del tuo saldo DISPONIBILE - SALDO: " << saldo << "$" << endl;
                          }
                          if (roulette[3].puntate < 0)
                          {
@@ -122,9 +120,9 @@ int main()
                          }
                     } while (roulette[3].puntate > saldo or roulette[3].puntate < 0);
                     saldo = saldo - roulette[3].puntate;
-                    cout << "Hai puntato: " << roulette[3].puntate << ", il tuo saldo adesso e' " << saldo << endl;
+                    cout << "Hai puntato: " << roulette[3].puntate << "$, il tuo saldo adesso e' " << saldo << "$" << endl;
                }
-               if (roulette[3].risp == 'e' or roulette[3].risp == 'E')
+               if (roulette[2].risp == 'e' or roulette[2].risp == 'E')
                {
                     cout << "Hai scelto la puntata per i numeri MAGGIORI di 18" << endl;
                     do
@@ -133,7 +131,7 @@ int main()
                          cin >> roulette[3].puntate;
                          if (roulette[3].puntate > saldo)
                          {
-                              cout << "Hai puntato piu' del tuo saldo DISPONIBILE - SALDO: " << saldo << endl;
+                              cout << "Hai puntato piu' del tuo saldo DISPONIBILE - SALDO: " << saldo << "$" << endl;
                          }
                          if (roulette[3].puntate < 0)
                          {
@@ -141,52 +139,62 @@ int main()
                          }
                     } while (roulette[3].puntate > saldo or roulette[3].puntate < 0);
                     saldo = saldo - roulette[3].puntate;
-                    cout << "Hai puntato: " << roulette[3].puntate << ", il tuo SALDO adesso e' " << saldo << endl;
+                    cout << "Hai puntato: " << roulette[3].puntate << "$, il tuo SALDO adesso e' " << saldo << "$" << endl;
                }
           }
 
+          system("CLS");
           cout << "E' uscito il numero: " << random << endl;
           if (random == roulette[0].puntate) // vincita su una puntata specifica
           {
                vincita = roulette[1].puntate * 35;
                saldo += vincita;
-               cout << "HAI VINTO: " << vincita << "; puntando sul " << roulette[0].puntate << endl;
+               cout << "HAI VINTO: " << vincita << "$"
+                    << "; puntando sul " << roulette[0].puntate << endl;
           }
 
           if ((random % 2 == 0) and (roulette[1].risp == 'p' or roulette[1].risp == 'P')) // vincita su una puntata pari
           {
                vincita = roulette[2].puntate * 2;
                saldo += vincita;
-               cout << "HAI VINTO: " << vincita << "; puntando sui numeri PARI" << endl;
+               cout << "HAI VINTO: " << vincita << "$"
+                    << "; puntando sui numeri PARI" << endl;
           }
 
           if ((random % 2 != 0) and (roulette[1].risp == 'd' or roulette[1].risp == 'D')) // vincita su una puntata dispari
           {
                vincita = roulette[2].puntate * 2;
                saldo += vincita;
-               cout << "HAI VINTO: " << vincita << "; puntando sui numeri DISPARI" << endl;
+               cout << "HAI VINTO: " << vincita << "$"
+                    << "; puntando sui numeri DISPARI" << endl;
           }
-          if ((random >= 18) and (roulette[3].risp == 'e' or roulette[3].risp == 'E'))
+          if ((random >= 18) and (roulette[2].risp == 'e' or roulette[2].risp == 'E'))
           {
                vincita = roulette[3].puntate * 2;
                saldo += vincita;
-               cout << "HAI VINTO: " << vincita << "; puntando sui numeri MAGGIORI di 18" << endl;
+               cout << "HAI VINTO: " << vincita << "$"
+                    << "; puntando sui numeri MAGGIORI di 18" << endl;
           }
-          if ((random <= 18) and (roulette[3].risp == 'g' or roulette[3].risp == 'G'))
+          if ((random <= 18) and (roulette[2].risp == 'g' or roulette[2].risp == 'G'))
           {
                vincita = roulette[3].puntate * 2;
                saldo += vincita;
-               cout << "HAI VINTO: " << vincita << "; puntando sui numeri MINORI di 18" << endl;
+               cout << "HAI VINTO: " << vincita << "$"
+                    << "; puntando sui numeri MINORI di 18" << endl;
           }
 
-          cout << "SALDO attuale: " << saldo << endl;
+          cout << "SALDO attuale: " << saldo << "$" << endl;
           cout << "Vuoi giocare ancora? (y/n)" << endl;
-          cin >> risposta;
+          cin >> roulette[3].risp;
 
-     } while (risposta == 'y' or risposta == 'Y' and saldo >= 1);
+     } while (roulette[3].risp == 'y' or roulette[3].risp == 'Y' and saldo >= 1);
      if (saldo <= 0)
      {
           cout << "Hai terminato il SALDO, se desideri rigiocare, recati alla cassa per ricaricare il SALDO." << endl;
      }
-     cout << "E' terminata la tua sessione di gioco, ecco il RIEPILOGO: \nSALDO d'entrata: " << saldoin << "\nSALDO d'uscita: " << saldo << endl;
+     if (roulette[3].risp == 'n' or roulette[3].risp == 'N' and saldo >= 1)
+     {
+          system("CLS");
+          cout << "E' terminata la tua sessione di gioco, ecco il RIEPILOGO: \nSALDO d'entrata: " << saldoin << "$\nSALDO d'uscita: " << saldo << "$" << endl;
+     }
 }
